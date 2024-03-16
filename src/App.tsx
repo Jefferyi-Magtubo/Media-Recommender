5e0bb86646b757fc4f8206b5df4c9e413106b622
jffryi <jefferyi.magtubo@gmail.com>
2024-03-16 05:54:49 -0500

    Removed unneeded imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 import SearchPage from './Pages/SearchPage'
